86c2cf092e54519250d3e78377772d986d0c43fd
Code Box <AJ@codes-macbook-air.local>
2016-10-10 10:45:08 -0400

    Initial Commit

---
+++ iBeaconTest3(PushNotifications)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconTest3(PushNotifications)
+//
+//  Created by Code Box on 10/10/16.
+//  Copyright © 2016 Uncle Austin's Premium Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

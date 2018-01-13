6f9b2e4d07036b18ec79311448cb919638737d0d
Sarcasm22 <austin.mccune@hcahealthcare.com>
2018-01-13 13:46:40 -0500

    test

--- iBeaconTest3(PushNotifications)/ViewController.swift
+++ iBeaconTest3(PushNotifications)/ViewController.swift
@@ -5,21 +5,50 @@
 //  Created by Code Box on 10/10/16.
 //  Copyright © 2016 Uncle Austin's Premium Corporation. All rights reserved.
 //
-
 import UIKit
+import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "A8731A74-4B9D-4F98-84F4-4377E66B760E")!, identifier: "Test Region")// fill in UUID of beacons
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse) {
+            
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        locationManager.startRangingBeacons(in: region)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        var notification : UINotificationFeedbackType = UINotificationFeedbackType()
+        
+        
+        
+    }
+    
+   /* func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{$0.proximity != CLProximity.unknown}
+        if (knownBeacons.count > 0) {
+    
 
-
+        }
+        
+    } */
+    
+    
 }
 
